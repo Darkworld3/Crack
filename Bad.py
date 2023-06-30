aedba2eb7428585c06e55ad76fe399496ffcbd32
Darkworld3 <134342598+Darkworld3@users.noreply.github.com>
2023-07-01 00:55:50 +0100

    Bad.py

---
+++ Bad.py
@@ -0,0 +1,41 @@
+# RANDOM 
+
+# PROGRAMMED BY OLAMI
+
+ # version 0.4
+
+# INSTALLATIONS
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install python`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install lolcat`
+- `pip install bs4`
+- `pkg install git`
+
+# MAIN CAMMANDS
+
+- `pip install rich`
+- `rm -rf Crack`
+- `git clone https://github.com/Darkworld3/Crack.git`
+- `cd Crack`
+- `python Bad.py`
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ [![Whatsapp](https://img.shields.io/badge/Whatsapp-OLAMI-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+2349031489012)
+
+
